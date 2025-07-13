4bed15233bc4b9a2d06f9acf05c99af109326730
sheltonc <shelton.dany@gmail.com>
2025-07-13 19:22:32 +0000

    dynamic and string lists

--- lists.go
+++ lists.go
@@ -8,5 +8,13 @@ func main() {
 	intList := []int{1, 2, 3, 4, 5}
 	fmt.Println(intList)
 	
+	stringList := []string{"hey now", "say now", "run up on my end"}
+	fmt.Println(stringList)
 
+	var dynamicList []int
+	dynamicList = make([]int, 3)
+	fmt.Println(dynamicList)
+
+	dynamicList = append(dynamicList, 4)
+	fmt.Println(dynamicList)
 }
