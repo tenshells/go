6aefcebaec338aff012317199a607505bce7f074
sheltonc <shelton.dany@gmail.com>
2025-07-14 06:37:13 +0000

    formatted prints

--- prints.go
+++ prints.go
@@ -6,4 +6,14 @@ import (
 
 func main() {
 	fmt.Print("Hello Go checkout-ers!\n")
+
+	someInt := 345
+	fmt.Println("One way to print number by ", someInt)
+
+	fmt.Printf("Another way to print variables: %v\n", someInt)
+	
+	someString := "BROTHER"
+	fmt.Println("multiple args accepted : ", someInt, " ", someString)
+
+	fmt.Printf("diff identifiers for string and int, string %s and int/others %v\n", someString, someInt)
 }
